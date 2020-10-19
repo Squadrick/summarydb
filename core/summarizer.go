3cef3b633bd7918d2825de9701f99efa399dd529
Dheeraj R Reddy <dheeraj98reddy@gmail.com>
2020-10-19 22:49:38 +0530

    Add Summarizer

---
+++ core/summarizer.go
@@ -0,0 +1,97 @@
+package core
+
+type Summarizer struct {
+	streamWindowManager *StreamWindowManager
+	windowLengths       []int64
+	numElements         int64
+}
+
+func NewSummarizer() *Summarizer {
+	return &Summarizer{
+		streamWindowManager: nil,
+		windowLengths:       nil,
+		numElements:         0,
+	}
+}
+
+func (s *Summarizer) SetWindowManager(manager *StreamWindowManager) {
+	s.streamWindowManager = manager
+}
+
+func (s *Summarizer) SetWindowLengths(windowLengths []int64) {
+	s.windowLengths = windowLengths
+}
+
+func (s *Summarizer) getNumWindowsCovering(ib *IngestBuffer) int {
+	if ib.IsFull() {
+		return len(s.windowLengths)
+	}
+	N := int64(0)
+	for w := 0; w < len(s.windowLengths); w += 1 {
+		N += s.windowLengths[w]
+		if N > ib.Size {
+			return w
+		} else if N == ib.Size {
+			return w + 1
+		}
+	}
+	return 0 // unreachable code
+}
+
+func (s *Summarizer) Run(
+	summarizerQueue <-chan *IngestBuffer,
+	writerQueue chan<- *SummaryWindow,
+	emptyBuffers chan<- *IngestBuffer,
+	partialBuffers chan<- *IngestBuffer) {
+
+	for {
+		select {
+		case ingestBuffer := <-summarizerQueue:
+			if ingestBuffer == ConstShutdownIngestBuffer() {
+				// notify (SUMMARIZER)
+				break
+			} else if ingestBuffer == ConstFlushIngestBuffer() {
+				// notify (SUMMARIZER)
+				continue
+			} else {
+				W := s.getNumWindowsCovering(ingestBuffer)
+				bs := int64(0)
+				be := int64(0)
+
+				for w := W - 1; w >= 0; w -= 1 {
+					be = bs + s.windowLengths[w] - 1
+					ts, okStart := ingestBuffer.GetTimestamp(bs)
+					te, okEnd := ingestBuffer.GetTimestamp(be)
+
+					if !(okStart && okEnd) {
+						continue
+					}
+
+					window := NewSummaryWindow(
+						ts, te, s.numElements+bs, s.numElements+be)
+
+					for c := bs; c <= be; c += 1 {
+						timestamp, okt := ingestBuffer.GetTimestamp(c)
+						value, okv := ingestBuffer.GetValue(c)
+						if !(okt && okv) {
+							continue
+						}
+						s.streamWindowManager.InsertIntoSummaryWindow(window, timestamp, value)
+					}
+
+					writerQueue <- window
+					bs = be + 1
+				}
+				s.numElements += bs
+
+				if bs == ingestBuffer.Size {
+					ingestBuffer.Clear()
+					emptyBuffers <- ingestBuffer
+				} else {
+					ingestBuffer.TruncateHead(bs)
+					partialBuffers <- ingestBuffer
+				}
+			}
+		}
+	}
+}
