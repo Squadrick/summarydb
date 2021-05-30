73ff031c7013ec46f5121537fd26ca741aadb5c6
Dheeraj R Reddy <dheeraj98reddy@gmail.com>
2021-05-30 23:10:20 +0530

    [DB] Simplify DB test

--- core/db_test.go
+++ core/db_test.go
@@ -7,46 +7,14 @@ import (
 	"testing"
 )
 
-func TestOpen(t *testing.T) {
+func TestBasicDB(t *testing.T) {
 	var streamId int64
 	{
 		db := New("testdb")
 		exp := window.NewExponentialLengthsSequence(2)
-		stream := db.NewStream([]string{"count"}, exp)
+		stream := db.NewStream([]string{"count", "sum"}, exp)
 		ctx, cancelFunc := context.WithCancel(context.Background())
 		// TODO: Let `db` start the stream, and keep track of the ctxs?
-		stream.Run(ctx)
-		streamId = stream.streamId
-		for i := 0; i < 100; i++ {
-			stream.Append(int64(i), 1.0)
-		}
-
-		err := db.Close()
-		cancelFunc()
-		assert.Equal(t, err, nil)
-	}
-	{
-		params := QueryParams{
-			ConfidenceLevel: 0.95,
-			SDMultiplier:    1.0,
-		}
-		db := Open("testdb")
-		stream := db.GetStream(streamId)
-
-		result := stream.Query("count", 0, 99, &params)
-		assert.Equal(t, result.value.Count.Value, 100.0)
-		assert.Equal(t, result.error, 0.0)
-
-	}
-}
-
-func TestOpen_Sum(t *testing.T) {
-	var streamId int64
-	{
-		db := New("testdb_sum")
-		exp := window.NewExponentialLengthsSequence(2)
-		stream := db.NewStream([]string{"sum"}, exp)
-		ctx, cancelFunc := context.WithCancel(context.Background())
 		stream.Run(ctx)
 		streamId = stream.streamId
 		for i := 0; i < 100; i++ {
@@ -62,12 +30,17 @@ func TestOpen_Sum(t *testing.T) {
 			ConfidenceLevel: 0.95,
 			SDMultiplier:    1.0,
 		}
-		db := Open("testdb_sum")
+		db := Open("testdb")
 		stream := db.GetStream(streamId)
-
-		result := stream.Query("sum", 0, 99, &params)
-		assert.Equal(t, result.value.Sum.Value, 99.0*100/2)
-		assert.Equal(t, result.error, 0.0)
-
+		{
+			result := stream.Query("count", 0, 99, &params)
+			assert.Equal(t, result.value.Count.Value, 100.0)
+			assert.Equal(t, result.error, 0.0)
+		}
+		{
+			result := stream.Query("sum", 0, 99, &params)
+			assert.Equal(t, result.value.Sum.Value, 99.0*100/2)
+			assert.Equal(t, result.error, 0.0)
+		}
 	}
 }
