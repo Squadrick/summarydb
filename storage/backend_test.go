6f039c11929450278fcb37b6aa9078cbb7140b1f
Dheeraj R Reddy <dheeraj98reddy@gmail.com>
2021-06-19 18:45:04 +0530

    Add missing error handling in tests

--- storage/backend_test.go
+++ storage/backend_test.go
@@ -6,12 +6,18 @@ import (
 )
 
 func testIterateIndex(t *testing.T, backend Backend) {
-	backend.Put(1, 1, nil)
-	backend.Put(1, 2, nil)
-	backend.Put(2, 3, nil)
-	backend.Put(2, 4, nil)
-	backend.PutLandmark(1, 5, nil)
-	backend.PutLandmark(1, 6, nil)
+	err := backend.Put(1, 1, nil)
+	assert.NoError(t, err)
+	err = backend.Put(1, 2, nil)
+	assert.NoError(t, err)
+	err = backend.Put(2, 3, nil)
+	assert.NoError(t, err)
+	err = backend.Put(2, 4, nil)
+	assert.NoError(t, err)
+	err = backend.PutLandmark(1, 5, nil)
+	assert.NoError(t, err)
+	err = backend.PutLandmark(1, 6, nil)
+	assert.NoError(t, err)
 
 	var index []int64
 
@@ -25,22 +31,26 @@ func testIterateIndex(t *testing.T, backend Backend) {
 	}
 
 	initIndex()
-	backend.IterateIndex(1, lambda, false)
+	err = backend.IterateIndex(1, lambda, false)
+	assert.NoError(t, err)
 	assert.Contains(t, index, int64(1))
 	assert.Contains(t, index, int64(2))
 
 	initIndex()
-	backend.IterateIndex(2, lambda, false)
+	err = backend.IterateIndex(2, lambda, false)
+	assert.NoError(t, err)
 	assert.Contains(t, index, int64(3))
 	assert.Contains(t, index, int64(4))
 
 	initIndex()
-	backend.IterateIndex(1, lambda, true)
+	err = backend.IterateIndex(1, lambda, true)
+	assert.NoError(t, err)
 	assert.Contains(t, index, int64(5))
 	assert.Contains(t, index, int64(6))
 
 	initIndex()
-	backend.IterateIndex(2, lambda, true)
+	err = backend.IterateIndex(2, lambda, true)
+	assert.NoError(t, err)
 	assert.Empty(t, index)
 }
 
